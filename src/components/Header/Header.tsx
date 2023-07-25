805bddde18ded154a36e9bc045f8beca5c363bad
sohodoll <kovalevvtl@gmail.com>
2023-07-25 15:39:50 +0300

    feat: new styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,12 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from './styles.module.css'
 
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <button>
-        <Link to='/timer'>Timer App</Link>
-      </button>
-      <button>
-        <Link to='/figma'>Figma Layout</Link>
-      </button>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/timer'>Timer App</NavLink>
+      <NavLink to='/figma'>Figma Layout</NavLink>
     </div>
   )
 }
--- src/pages/Figma/Figma.tsx
+++ src/pages/Figma/Figma.tsx
@@ -1,6 +1,9 @@
+import { Header } from 'components'
+
 export const Figma = () => {
   return (
     <div>
+      <Header />
       <h1>Figma</h1>
     </div>
   )
