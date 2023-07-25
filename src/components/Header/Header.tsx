87cc33d7a3c953d57aaa9f1c4caf59a2e369f7d3
sohodoll <kovalevvtl@gmail.com>
2023-07-25 14:15:17 +0300

    feat: initialize project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <button>Timer App</button>
+      <button>Figma Layout</button>
+    </div>
+  )
+}
